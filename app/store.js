6294907b7aaaf9313561fe0c8f0ffe8b99733d22
Rob Poitras <rbpoitras@gmail.com>
2017-07-01 07:36:15 -0400

    upgrade to webpack 3, add HMR for Redux

--- app/store.js
+++ app/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import createBrowserHistory from 'history/createBrowserHistory'
-
 import rootReducer, { getDefaultInitialState } from './reducers'
 
 // Initial Redux Store state is pulled from each reducer's initial state definition.
@@ -18,6 +17,15 @@ if (process.env.NODE_ENV === 'development') {
 } else {
   composeEnhancers = compose
 }
-export const store = createStore(rootReducer, initialState, composeEnhancers(
+const store = createStore(rootReducer, initialState, composeEnhancers(
   applyMiddleware(historyRouterMiddleware)
 ))
+
+if (module.hot) {
+  module.hot.accept('./reducers', () => {
+    const nextReducer = require('./reducers/index').default
+    store.replaceReducer(nextReducer)
+  })
+}
+
+export default store
