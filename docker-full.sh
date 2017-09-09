14f279ebb4c8190e6f393e17f4292617348fdd2c
Rob Poitras <rbpoitras@gmail.com>
2017-09-09 19:20:38 -0400

    nginx ssl, lib updates

--- docker-full.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker stop react-universal
-docker container rm react-universal
-docker image rm react-universal-prod-i
-yarn run clean
-yarn run build
-docker build -t react-universal-prod-i .
-docker run -d --name react-universal -p 8090:8090 react-universal-prod-i
