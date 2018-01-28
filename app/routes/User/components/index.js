5b6da608fd272daff1bbaddc07d14814f7896c27
Rob Poitras <rbpoitras@gmail.com>
2018-01-28 09:03:46 -0500

    add user table

--- app/routes/User/components/index.js
+++ app/routes/User/components/index.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
 
 class User extends Component {
   static propTypes = {
@@ -7,9 +15,23 @@ class User extends Component {
     actions: PropTypes.object.isRequired
   }
 
-  // constructor (props) {
-  //   super(props)
-  // }
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      selected: [1]
+    }
+  }
+
+  isSelected = (index) => {
+    return this.state.selected.indexOf(index) !== -1
+  }
+
+  handleRowSelection = (selectedRows) => {
+    this.setState({
+      selected: selectedRows
+    })
+  }
 
   componentDidMount () {
     this.props.actions.fetchUsers()
@@ -20,22 +42,48 @@ class User extends Component {
     console.log('users: ', this.props.users)
   }
 
-  renderList = (users) => {
+  renderRows = (users) => {
     if (!users || users.size < 1) {
-      return <li>Loading...</li>
+      return (
+        <TableRow selected={this.isSelected(0)}>
+          <TableRowColumn>Loading...</TableRowColumn>
+        </TableRow>
+      )
     }
-    console.log('User list:', users)
     return users.map((user, index) => {
-      return <li key={index}>{user.name}</li>
+      return (
+        <TableRow key={index} selected={this.isSelected(index)}>
+          <TableRowColumn>{user.id}</TableRowColumn>
+          <TableRowColumn>{user.name}</TableRowColumn>
+          <TableRowColumn>{user.role}</TableRowColumn>
+        </TableRow>
+      )
     })
+  }
+
+  renderTable = (users) => {
+    return (
+      <Table onRowSelection={this.handleRowSelection} multiSelectable={false}>
+        <TableHeader>
+          <TableRow>
+            <TableHeaderColumn>ID</TableHeaderColumn>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Role</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {this.renderRows(users)}
+        </TableBody>
+      </Table>
+    )
   }
 
   render () {
     const { users } = this.props
     return (
-      <div>
-        <h1>This will be the User Page</h1>
-        <ul>{this.renderList(users)}</ul>
+      <div className='column-container'>
+        <h1>Users</h1>
+        {this.renderTable(users)}
       </div>
     )
   }
